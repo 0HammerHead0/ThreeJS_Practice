a2d7215128beadf48e71bcdff169005809ff9bdb
0HammerHead0 <shashwatakahammer@gmail.com>
2023-12-16 20:29:32 +0530

    initial commit

--- src/script/app.js
+++ src/script/app.js
@@ -1,9 +1,17 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 import '../style/app.css';
+
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight 
+}
+console.log('Hello Webpack Encore! Edit me in assets/app.js');
+console.log(gsap);
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
@@ -21,4 +29,4 @@ function animate() {
     cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
-animate();
+// animate();
